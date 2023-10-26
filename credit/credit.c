c334ee95f58b846f4f2c124ffa50affdd4a386cb
Tiago <50381798+devve2kccc@users.noreply.github.com>
2023-10-26 11:29:48 +0000

    modified:   .vscode/launch.json 	modified:   .vscode/tasks.json 	new file:   credit/Makefile 	modified:   credit/credit.c

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,26 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int credit_digits(long credit_card);
+
 int main(void) 
 {
+    long number = get_long("Number: ");
+    
+    int numb = credit_digits(number);
+    printf("%i\n", numb);
+    
+}
+
+int credit_digits(long credit_card)
+{
+    int sum = 0;
+    int last_digit;
+    while (credit_card > 0)
+    {
+        last_digit = credit_card % 10;
+        credit_card = credit_card / 10;
+    }
+    return sum;
     
 }
